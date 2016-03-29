f6cc0e87371bef9dc229eae0df7ef96005ee05cd
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-03-28 20:32:12 -0700

    Removed the main to compile with the test file

--- c-intro/madlib.c
+++ c-intro/madlib.c
@@ -4,13 +4,7 @@
 
 char* madlib(char* template, char* adjective, char* noun, char* verb) {
     int stringLength = strlen(template) + strlen(adjective) + strlen(noun) + strlen(verb);
-    char* madlibString = malloc(stringLength + 1);
+    char* madlibString = (char*)malloc(sizeof(char) * stringLength + 1);
     sprintf(madlibString, template, adjective, noun, verb);
     return madlibString;
-}
-
-int main(int argc, char* argv[]) {
-    printf("%s \n", madlib(argv[1], argv[2], argv[3], argv[4]));
-    
-    return 0;
 }
