abd195f0511824536c4244a729dc0a3224966958
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-03-29 15:57:10 -0700

    First inital (working) commit of reverse-words.c

---
+++ c-intro/reverse-words.c
@@ -0,0 +1,25 @@
+
+
+#include "reverse-range-in-place.h"
+#include <string.h>
+#include <stdio.h>
+
+void reverse_words(char* string) {
+    
+    int start_index = 0;
+    int end_index = 0;
+    
+    for (int i = 0; i < strlen(string); i++) {
+        if (string[i] == ' ') {
+            reverse_range_in_place(string, start_index, end_index - 1);
+            start_index = i + 1;
+            end_index = i + 1;
+        } else {
+            end_index++;
+        }
+    }
+    reverse_range_in_place(string, start_index, end_index);
+    reverse_range_in_place(string, 0, strlen(string));
+    
+}
+
