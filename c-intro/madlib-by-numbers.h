a885531be649f9fa9379da244f81b964a28f77cd
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-03-29 01:32:24 -0700

    header file for madlib-by-numbers.h

---
+++ c-intro/madlib-by-numbers.h
@@ -0,0 +1,10 @@
+#ifndef MADLIB_BY_NUMBERS
+#define MADLIB_BY_NUMBERS
+
+int findNewLength(char* template, int word_count, char* words[]);
+
+char* madlib_by_numbers(char* template, int word_count, char* words[]);
+
+
+
+#endif /* madlib_by_numbers_h */
