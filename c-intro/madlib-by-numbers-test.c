7722ed6ddc4974b7a3d00062c275ce7a3ded6009
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-03-29 13:38:52 -0700

    Added a new test and moved the free(original)

--- c-intro/madlib-by-numbers-test.c
+++ c-intro/madlib-by-numbers-test.c
@@ -4,17 +4,31 @@
 #include <ctype.h>
 #include "madlib-by-numbers.h"
 
+#include <stdio.h>
 
 int main() {
     char* template = "The 1 2 likes to 9 in the moonlight.";
-    char* words_to_use[]= {"swim", "brilliant", "frog"};
-    int word_count = 3;
+    char* words_to_use[]= {"swim", "brilliant", "frog", "hi"};
+    int word_count = 4;
     char* madlibS = "The brilliant frog likes to 9 in the moonlight.";
     int length = findNewLength(template, word_count, words_to_use);
-    int mlLegnth = length;
+    int mlLength = length;
     
     char* original = madlib_by_numbers("The 1 2 likes to 9 in the moonlight.", word_count, words_to_use);
-    assert(strncmp(madlibS, original, mlLegnth) == 0);
+    assert(strncmp(madlibS, original, mlLength) == 0);
+    
+    
+    char* template2 = "The 1 2 likes to 3 in the moonlight.";
+    char* words_to_use2[]= {"swim", "brilliant", "frog", "hi"};
+    int word_count2 = 4;
+    char* madlibS2 = "The brilliant frog likes to hi in the moonlight.";
+    int length2 = findNewLength(template2, word_count2, words_to_use2);
+    int mlLength2 = length2;
+    
+    char* original2 = madlib_by_numbers(template2, word_count2, words_to_use2);
+    
+    assert(strncmp(madlibS2, original2, mlLength2) == 0);
     free(original);
-     
+    free(original2);
 }
+
