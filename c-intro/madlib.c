f6d713fccdc69b9f7d9dd583996b29bc48269075
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-03-29 23:48:24 -0700

    Corrected for the edge case if they give us more "%s's" than we have words for (more than 3).

--- c-intro/madlib.c
+++ c-intro/madlib.c
@@ -3,8 +3,23 @@
 #include <string.h>
 
 char* madlib(char* template, char* adjective, char* noun, char* verb) {
-    int stringLength = strlen(template) + strlen(adjective) + strlen(noun) + strlen(verb);
-    char* madlibString = (char*)malloc(sizeof(char) * stringLength + 1);
-    sprintf(madlibString, template, adjective, noun, verb);
-    return madlibString;
+    int count = 0;
+    char* newTemplate = template;
+    while (newTemplate < (template + strlen(template))) {
+        newTemplate = strstr(newTemplate, "%s");
+        if (newTemplate == NULL) {
+            break;
+        }
+        count++;
+        newTemplate = newTemplate + 2;
+    }
+    if (count <= 3) {
+        int stringLength = strlen(template) + strlen(adjective) + strlen(noun) + strlen(verb);
+        char* madlibString = (char*)malloc(sizeof(char) * stringLength + 1);
+        sprintf(madlibString, template, adjective, noun, verb);
+        return madlibString;
+    }
+    else {
+        return 0;
+    }
 }
