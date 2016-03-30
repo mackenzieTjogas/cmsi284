a9285eed763ba68413dc0e3dccd4849dd4b7f4ac
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-03-29 22:53:47 -0700

    Added more tests.

--- c-intro/madlib-by-numbers-test.c
+++ c-intro/madlib-by-numbers-test.c
@@ -1,9 +1,7 @@
 #include <assert.h>
 #include <string.h>
 #include <stdlib.h>
-#include <ctype.h>
 #include "madlib-by-numbers.h"
-
 #include <stdio.h>
 
 int main() {
@@ -26,9 +24,43 @@ int main() {
     int mlLength2 = length2;
     
     char* original2 = madlib_by_numbers(template2, word_count2, words_to_use2);
-    
     assert(strncmp(madlibS2, original2, mlLength2) == 0);
+
+    char* template3 = "0 3 nice weather we're 1.";
+    char* words_to_use3[]= {"Hello", "having", "frog", "there"};
+    int word_count3 = 4;
+    char* madlibS3 = "Hello there nice weather we're having.";
+    int length3 = findNewLength(template3, word_count3, words_to_use3);
+    int mlLength3 = length3;
+    
+    char* original3 = madlib_by_numbers(template3, word_count3, words_to_use3);
+    assert(strncmp(madlibS3, original3, mlLength3) == 0);
+    
+    char* template4 = "0 6 nice weather we're 1.";
+    char* words_to_use4[]= {"Hello", "having", "frog", "there"};
+    int word_count4 = 4;
+    char* madlibS4 = "Hello 6 nice weather we're having.";
+    int length4 = findNewLength(template4, word_count4, words_to_use4);
+    int mlLength4 = length4;
+    
+    char* original4 = madlib_by_numbers(template4, word_count4, words_to_use4);
+    assert(strncmp(madlibS4, original4, mlLength4) == 0);
+    
+    char* template5 = "0 612 nice weather we're 1.";
+    char* words_to_use5[]= {"Hello", "having", "frog", "there"};
+    int word_count5 = 4;
+    char* madlibS5 = "Hello 6havingfrog nice weather we're having.";
+    int length5 = findNewLength(template5, word_count5, words_to_use5);
+    int mlLength5 = length5;
+    
+    char* original5 = madlib_by_numbers(template5, word_count5, words_to_use5);
+    assert(strncmp(madlibS5, original5, mlLength5) == 0);
+
     free(original);
     free(original2);
+    free(original3);
+    free(original4);
+    free(original5);
 }
+
 
