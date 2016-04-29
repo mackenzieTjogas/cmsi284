390b2880f881f87993e025a1a395929a09a0da71
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-04-29 15:03:26 -0700

    Fixed spacing errors for rev-echo.asm

--- asm-intro/rev-echo.asm
+++ asm-intro/rev-echo.asm
@@ -7,23 +7,23 @@
 main:
   push    rdi		                	;rdi holds the string 
   push    rsi			                ;rsi holds the starting index
-	sub     rsp, 8         		      ;align the stack before call
+  sub     rsp, 8         		      ;align the stack before call
 
   mov     rdi, [rsi]      	      ;the argument string to display
   call	  strlen			            ;length of the word
-	mov	    rdx, rax		            ;put end index into rdx	(strlen stores it in rax)
-	mov	    rdi, [rsi]		          ;put the string into rdi
-	mov	    rsi, 0			            ;put the starting index, 0, into rsi
-	push	  rdi
-	call	  reverse_range_in_place
-	pop	    rdi
+  mov	  rdx, rax		            ;put end index into rdx	(strlen stores it in rax)
+  mov	  rdi, [rsi]		          ;put the string into rdi
+  mov	  rsi, 0			            ;put the starting index, 0, into rsi
+  push	  rdi
+  call	  reverse_range_in_place
+  pop	  rdi
  	
-	call    puts                    ; print it
-	add     rsp, 8                  ; restore %rsp to pre-aligned value
+  call    puts                    ; print it
+  add     rsp, 8                  ; restore %rsp to pre-aligned value
   pop     rsi                     ; restore registers puts used
- 	pop	    rdi
+  pop	  rdi
         
-	add     rsi, 8                  ; point to next argument
+  add     rsi, 8                  ; point to next argument
   dec     rdi                     ; count down
   jnz     main                    ; if not done counting keep going
 
