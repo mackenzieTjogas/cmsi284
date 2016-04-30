b238b23510d4c4f16eda4614f17aaec43e3c96da
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-04-29 18:12:32 -0700

    Fixed the spacing for madlibs.asm

--- asm-intro/madlibs/malibs.asm
+++ asm-intro/madlibs/malibs.asm
@@ -5,15 +5,15 @@
 	section	.text
 
 main:
-	push 	  rbx			
+	push 	    rbx			
 	mov	    rcx, rdi		    ;put the words to add into rcx
-	sub	    rcx, 2			    ;need to subtract for ./a.out and the template
-	mov	    rdi, [rsi+8]		;template goes into rdi
-	add	    rsi, 16			    ;words to add goes into rsi
+	sub	    rcx, 2	            ;need to subtract for ./a.out and the template
+	mov	    rdi, [rsi+8]	    ;template goes into rdi
+	add	    rsi, 16	            ;words to add goes into rsi
 	mov	    rdx, rsi		    ;put the words to add into rdx
 	mov	    rsi, rcx		    
-	call	  madlib_by_numbers	
+	call	    madlib_by_numbers	
 	mov	    rdi, rax		    ;put the result into rdi 
-	call	  puts			      ;print 
+	call	    puts	            ;print 
 	pop	    rbx			
 	ret
