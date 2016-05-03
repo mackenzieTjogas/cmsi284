bca87f7a8922369a743c8eb7da4eaab1cab145f4
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-05-02 18:48:58 -0700

    Had the incorrect system call (write instead of rename) for sys_rename.asm

--- asm-intro/sys_rename.asm
+++ asm-intro/sys_rename.asm
@@ -1,16 +1,15 @@
 
-	      global	main
+	global	main
 	
-	      section	.text
+	section	.text
 main:
 					
-        mov     rax, 1                	; system call 1 is write
+        mov     rax, 82                	; system call number to rename is 82
         mov     rdi, [rsi + 8]          ; move to next argument, the new name
-        mov     rsi, [rsi + 16]         ; address of string to output
+        mov     rsi, [rsi + 16]         ; second argument, address of string to output
 
         syscall                         ; invoke operating system to do the write
 
-        ; exit(0)
         mov     eax, 60                 ; system call 60 is exit
         xor     rdi, rdi                ; exit code 0
         syscall                         ; invoke operating system to exit
