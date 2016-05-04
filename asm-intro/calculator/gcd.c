947830335fffb7d15a6eb9826afc790e5ece10c9
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-05-03 23:12:21 -0700

    Fixed some minor errors for gcd.c

--- asm-intro/calculator/gcd.c
+++ asm-intro/calculator/gcd.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-int64_t gcd(int 64_t, int64_t);
+int64_t gcd(int64_t, int64_t);
 
 int main(int argc, char** argv) {
 	if (argc != 3) {
-		printf(This is the wrong number of arguments \n");
+		printf("This is the wrong number of arguments \n");
 		return 0;
 	}
 
 	int firstArg = atoi(argv[1]);
 	int secondArg = atoi(argv[2]);
-	int answer = gcd(firstArg, secondArg);
-	printf("%ld\n", answer);
+	printf("%ld\n", gcd(firstArg, secondArg));
 }
