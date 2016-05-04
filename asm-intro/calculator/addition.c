4b77e61ab995a2caca8667e73d43b1647e27b194
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-05-03 23:10:18 -0700

    Fixed some minor errors for addition.c

--- asm-intro/calculator/addition.c
+++ asm-intro/calculator/addition.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-int64_t addition(int 64_t, int64_t);
+int64_t addition(int64_t, int64_t);
 
 int main(int argc, char** argv) {
 	if (argc != 3) {
-		printf(This is the wrong number of arguments \n");
+		printf("This is the wrong number of arguments \n");
 		return 0;
 	}
 
 	int firstArg = atoi(argv[1]);
 	int secondArg = atoi(argv[2]);
-	int sum = addition(firstArg, secondArg);
-	printf("%ld\n", sum);
+	printf("%ld\n", addition(firstArg, secondArg));
 }
