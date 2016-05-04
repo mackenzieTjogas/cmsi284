1798e22ab72e26f2a772307412d27d5f0ca3d5d1
Mackenzie Tjogas <mtjogas@lion.lmu.edu>
2016-05-03 23:13:42 -0700

    Fixed some minor errors for subtract.c

--- asm-intro/calculator/subtract.c
+++ asm-intro/calculator/subtract.c
@@ -2,15 +2,14 @@
 #include <stdlib.h>
 #include <inttypes.h>
 
-int64_t subtract(int 64_t, int64_t);
+int64_t subtract(int64_t, int64_t);
 
 int main(int argc, char** argv) {
 	if (argc != 3) {
-		printf(This is the wrong number of arguments \n");
+		printf("This is the wrong number of arguments \n");
 		return 0;
 	}
 	int firstArg = atoi(argv[1]);
 	int secondArg = atoi(argv[2]);
-	int result = subtract(firstArg, secondArg);
-	printf("%ld\n", result);
+	printf("%ld\n", subtract(firstArg, secondArg));
 }
